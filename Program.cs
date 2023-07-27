dda5f44f654a4c3fbb1b078229770a9f735c70a5
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2023-07-27 08:31:06 +0500

    Changed Readme file and added program continue

--- Program.cs
+++ Program.cs
@@ -3,45 +3,50 @@ using System.Threading.Tasks.Dataflow;
 
 namespace MainSpace;
 
-    public class Program{
-        public static void Main(string[] args)
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Choose option 1 or 2");
+        Console.WriteLine("1 - > convert uzbek soum to dollar");
+        Console.WriteLine("2 - > convert dollar to uzbek soum");
+
+        string optionString = Console.ReadLine();
+
+        decimal amountOfMoney = 0;
+        decimal resultAmount = 0;
+
+        if (optionString == "1")
         {
-            Console.WriteLine("Choose options 1 or 2");
-            Console.WriteLine("1 - > convert uzbek soum to dollar");
-            Console.WriteLine("2 - > convert dollar to uzbek soum");
 
-            string optionString  = Console.ReadLine();    
-            float option = float.Parse(optionString); 
+            Console.WriteLine("Currently dollar course is 11600");
+            Console.WriteLine("Enter the soum");
 
-            decimal amountOfMoney = 0;
-            decimal resultAmount = 0;
+            amountOfMoney = decimal.Parse(Console.ReadLine());
+            resultAmount = amountOfMoney / 11600;
 
-            if(option == 1){
+            Console.WriteLine($"{amountOfMoney} soum is equal to {resultAmount} dollar");
+        }
+        else if (optionString == "2")
+        {
+            Console.WriteLine("Currently dollar course is 11600");
+            Console.WriteLine("Enter the dollar");
 
-                Console.WriteLine("Currently dollar course is 11600");
-                Console.WriteLine("Enter the soum");
+            amountOfMoney = decimal.Parse(Console.ReadLine());
+            resultAmount = amountOfMoney * 11600;
 
-                amountOfMoney = decimal.Parse(Console.ReadLine());
-                resultAmount = amountOfMoney / 11600    ;
-
-                Console.WriteLine($"{amountOfMoney } soum is equal to {resultAmount} dollar");
-                
-            }
-            else if(option == 2){
-                Console.WriteLine("Currently dollar course is 11600");
-                Console.WriteLine("Enter the dollar");
-
-                amountOfMoney = decimal.Parse(Console.ReadLine());
-                resultAmount = amountOfMoney * 11600    ;
-
-                Console.WriteLine($"{amountOfMoney } dollar is equal to {resultAmount} soum");
-            }
-            else{
-                Console.WriteLine("You have choosed wrong option, please try again");
-            }
-
-            Console.WriteLine("That is it");
-        
+            Console.WriteLine($"{amountOfMoney} dollar is equal to {resultAmount} soum");
         }
 
+        Console.WriteLine("Press 1 to continue or anything to quit");
+        
+        string option = Console.ReadLine();
+        if (option == "1")
+            Main(args);
+        else return;
+
+        Console.WriteLine("That is it");
+
     }
+
+}
--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Doviz
-Ultimate exchange tool to convert the value of dollar to uzbek soum and vice versa.
-
-
+### Ultimate exchange tool to convert the value of dollar to uzbek soum and vice versa.
 
 * Current value of dollar as soum 
 
-![Alt text](image.png)
 
-* This is how project works as gif
-
-![Alt text](<ezgif.com-video-to-gif (1).gif>)
+![!\[Alt text\](./Assets/Images/image.png)](Assets/Images/image.png)
 
 * This is how project works as video
 
-[Vide experience](c:/Users/zzrxm/Videos/IMG_2521.MOV)
+<video src="Assets/Gifs/de9fd89d-a572-4773-a867-e682418415e9.mp4" controls title="!\[Alt text\](<ezgif.com-video-to-gif (1).gif>)"></video>
+
+
+
+* This is how project works as gif
+
+
+
+![\[Vide experience\](c:/Users/zzrxm/Videos/IMG_2521.MOV)](<Assets/Images/ezgif.com-video-to-gif (1).gif>)
+
+* [You can find the source code here](https://github.com/Aziz7574/Doviz)
